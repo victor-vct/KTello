c1d2df169cc5963772430e4e289d879b652a7e05
Victor Vieira <victorvieira302@gmail.com>
2019-03-12 00:06:57 -0300

    feat(sample): add basic button to test flight control
    Signed-off-by: Victor Vieira <victorvieira302@gmail.com>

--- app/src/main/java/dev/victorvieira/sampleapp/MainActivity.kt
+++ app/src/main/java/dev/victorvieira/sampleapp/MainActivity.kt
@@ -2,11 +2,32 @@ package dev.victorvieira.sampleapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import dev.victorvieira.ktello.ktello
+import kotlinx.android.synthetic.main.activity_main.testFlightButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        testFlightButton.setOnClickListener {
+            testFlight()
+        }
+
+    }
+
+    private fun testFlight() {
+        ktello {
+            connect()
+
+            takeoff()
+
+            Thread.sleep(5000)
+            rotateClockwise(180)
+
+            Thread.sleep(5000)
+            land()
+        }
     }
 }
